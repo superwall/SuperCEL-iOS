d388623ae783bff287fbbefc636a15bb72085012
Yusuf Tör <3296904+yusuftor@users.noreply.github.com>
2024-08-09 15:32:30 +0200

    First commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "SuperCEL",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "SuperCEL",
+      targets: ["SuperCEL"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "SuperCEL",
+      dependencies: [
+        .target(name: "libcel")
+      ]
+    ),
+    .binaryTarget(
+      name: "libcel",
+      path: "./Frameworks/libcel.xcframework"
+    ),
+    .testTarget(
+      name: "SuperCELTests",
+      dependencies: ["SuperCEL"]),
+  ]
+)
